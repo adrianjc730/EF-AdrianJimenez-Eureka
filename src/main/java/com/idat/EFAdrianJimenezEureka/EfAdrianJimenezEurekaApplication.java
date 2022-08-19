006dbc94a2accfb9429c7ebacea640be914d2c3c
adrianjc730 <adrianjc730@gmail.com>
2022-08-18 22:26:18 -0500

    Eureka

---
+++ src/main/java/com/idat/EFAdrianJimenezEureka/EfAdrianJimenezEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EFAdrianJimenezEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAdrianJimenezEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAdrianJimenezEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/EFAdrianJimenezEureka/EfAdrianJimenezEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFAdrianJimenezEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfAdrianJimenezEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
